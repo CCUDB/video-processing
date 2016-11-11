6bbaad2b8868b5d3cc9a182448f944ae00a231d0
FATESAIKOU <qzsecftbhhhh@gmail.com>
2016-11-11 13:39:16 +0800

    Add slicement for first chunk.

--- src/VideoProcessKernel/VideoContent.php
+++ src/VideoProcessKernel/VideoContent.php
@@ -13,7 +13,9 @@ class VideoContent
 {
     private $real_file_path;
     private $file_name;
+    private $file_offset;
     private $file_id;
+    private $first_chunk_id;
     private $counter; // from 1 to n
 
     private $conn;
@@ -28,8 +30,10 @@ class VideoContent
 
         $this->real_file_path = VIDEO_STORAGE . '/' . $file_name;
         $this->file_name = $file_name;
+        $this->file_offset = $offset;
         $this->file_id = $this->meta_table->get($this->real_file_path)->run($this->conn)['video_id'];
-        $this->counter = (int)(ceil($offset / CHUNK_SIZE));
+        $this->first_chunk_id = (int)(ceil($offset / CHUNK_SIZE));
+        $this->counter = $this->first_chunk_id;
     }
 
     function __destruct() {
@@ -37,7 +41,9 @@ class VideoContent
     }
 
     public function setOffset($offset) {
-        $this->counter = (int)(ceil($offset / CHUNK_SIZE));
+        $this->file_offset = $offset;
+        $this->first_chunk_id = (int)(ceil($offset / CHUNK_SIZE));
+        $this->counter = $this->first_chunk_id;
     }
 
     public function nextContent() {
@@ -78,13 +84,16 @@ class VideoContent
             $this->content_table->get($content_id)->update(['chunk' => $insert_data])->run($this->conn);
             
             // Reget content again.
-            $content = $this->content_table->get($content_id)->run($this->conn);
+            $content = $this->getContent($content_id);
         } else if ($content['chunk'] === NULL) {
             // If content is in updating, wait for it.
             $this->content_table->get($content_id)->changes()->run($this->conn)->next();
 
             // Reget content again.
-            $content = $this->content_table->get($content_id)->run($this->conn);
+            $content = $this->getContent($content_id);
+        } else if ($file->counter === $file->first_chunk_id) {
+            $chunk_offset = $file_offset - ($this->first_chunk_id * CHUNK_SIZE);
+            $content = $this->content_table->get($content_id)->slice($chunk_offset)->run($this->conn);
         }
 
         // Counter Increment. 
@@ -92,6 +101,15 @@ class VideoContent
 
         // Return data.
         return $content['chunk'];
+    }
+
+    private function getContent($content_id) {
+        if ($this->counter != $this->first_chunk_id) {
+            return $this->content_table->get($content_id)->run($this->conn);
+        } else {
+            $chunk_offset = $file_offset - ($this->first_chunk_id * CHUNK_SIZE);
+            return $this->content_table->get($content_id)->slice($chunk_offset)->run($this->conn);
+        }
     }
 
     public function dumpVars() {
